b824dd2703f82b19fed77942eccdc1437b568c65
Matthew Gasbarro <matthew.gasbarro@gmail.com>
2020-05-08 11:29:21 -0400

    refactored checking for valid moves

--- Othello/OthelloBoard.py
+++ Othello/OthelloBoard.py
@@ -7,6 +7,11 @@ class OthelloBoard(Board):
 
         self.__pieceCount = {"B":2,"W":2,str(self._delimeter):(self._boardSize**2)-4}
         self.__tmpMove = []
+
+        dirCoord = [-1,0,1]
+        self.__dirList = [[x,y] for x in dirCoord for y in dirCoord]
+        self.__dirList.remove([0,0])
+
         self._initBoard()
 
     # PROPERTIES
@@ -14,6 +19,10 @@ class OthelloBoard(Board):
     @property
     def PieceCount(self):
         return self.__pieceCount
+
+    @staticmethod
+    def Opp(turn):
+        return "B" if turn == "W" else "W"
 
     # PUBLIC METHODS
 
@@ -69,141 +78,40 @@ class OthelloBoard(Board):
             self.__tmpMove = []
 
     # PRIVATE METHODS
+    def __isValidMoveDir(self, coord, coordDir, opp):
+        Y = int(coord[0])
+        X = int(coord[1])
+
+        Yinc = int(coordDir[0])
+        Xinc = int(coordDir[1])
+
+        tmpMove = []
+        myPieceFound = False
+        
+        for i in range(1, self._boardSize-1):
+            Yj = (i * Yinc if Yinc != 0 else 0) + Y
+            Xj = (i * Xinc if Xinc != 0 else 0) + X
+
+            if Yj < self._boardSize and Xj < self._boardSize:
+                if self._gameBoard[Yj][Xj] == opp:
+                    tmpMove.append([Yj,Xj])
+                elif self._gameBoard[Yj][Xj] == self._delimeter:
+                    break
+                else:
+                    myPieceFound = True
+
+        if myPieceFound and tmpMove:
+            self.__tmpMove.extend(tmpMove)
 
     # check all 8 directions for valid move and store flips in tmp list
     def __isValidMove(self, turn, coord):
         if  not self.__tmpMove:
             Y = int(coord[0])
             X = int(coord[1])
-            opp = "B" if turn == "W" else "W"
+            opp = self.Opp(turn)
             tmpMove = []
-
-            # Check horizontal right
-            myPieceFound = False
-            if X < self._boardSize - 2 and self._gameBoard[Y][X+1] == opp:
-                for i in range(X+1, self._boardSize):
-                    if self._gameBoard[Y][i] == opp:
-                        tmpMove.append([Y,i])
-                    elif self._gameBoard[Y][i] == self._delimeter:
-                        break
-                    else:
-                        myPieceFound = True
-
-            if myPieceFound and tmpMove:
-                self.__tmpMove.extend(tmpMove)
-            tmpMove.clear()
-
-            # Check horizontal left
-            myPieceFound = False
-            if X > 1 and self._gameBoard[Y][X-1] == opp:
-                for i in range(X-1, -1, -1):
-                    if self._gameBoard[Y][i] == opp:
-                        tmpMove.append([Y,i])
-                    elif self._gameBoard[Y][i] == self._delimeter:
-                        break
-                    else:
-                        myPieceFound = True
-
-            if myPieceFound and tmpMove:
-                self.__tmpMove.extend(tmpMove)
-            tmpMove.clear()
             
-            # Check vertical down
-            myPieceFound = False
-            if Y < self._boardSize - 2  and self._gameBoard[Y+1][X] == opp:
-                for i in range(Y+1, self._boardSize):
-                    if self._gameBoard[i][X] == opp:
-                        tmpMove.append([i,X])
-                    elif self._gameBoard[i][X] == self._delimeter:
-                        break
-                    else:
-                        myPieceFound = True
-
-            if myPieceFound and tmpMove:
-                self.__tmpMove.extend(tmpMove)
-            tmpMove.clear()
-
-            # Check vertical up
-            myPieceFound = False
-            if Y > 1  and self._gameBoard[Y-1][X] == opp:
-                for i in range(Y-1, -1, -1):
-                    if self._gameBoard[i][X] == opp:
-                        tmpMove.append([i,X])
-                    elif self._gameBoard[i][X] == self._delimeter:
-                        break
-                    else:
-                        myPieceFound = True
-
-            if myPieceFound and tmpMove:
-                self.__tmpMove.extend(tmpMove)
-            tmpMove.clear()
-
-            # Check diagonal up and right
-            myPieceFound = False
-            if X < self._boardSize - 2 and Y > 1 and self._gameBoard[Y-1][X+1] == opp:
-                maxMve = min(Y, self._boardSize-1-X)
-
-                for i in range(1, maxMve+1):
-                    if self._gameBoard[Y-i][X+i] == opp:
-                        tmpMove.append([Y-i,X+i])
-                    elif self._gameBoard[Y-i][X+i] == self._delimeter:
-                        break
-                    else:
-                        myPieceFound = True
-
-            if myPieceFound and tmpMove:
-                self.__tmpMove.extend(tmpMove)
-            tmpMove.clear()
-
-            # Check diagonal up and left
-            myPieceFound = False
-            if X > 1 and Y > 1 and self._gameBoard[Y-1][X-1] == opp:
-                maxMve = min(Y, X)
-
-                for i in range(1, maxMve+1):
-                    if self._gameBoard[Y-i][X-i] == opp:
-                        tmpMove.append([Y-i,X-i])
-                    elif self._gameBoard[Y-i][X-i] == self._delimeter:
-                        break
-                    else:
-                        myPieceFound = True
-
-            if myPieceFound and tmpMove:
-                self.__tmpMove.extend(tmpMove)
-            tmpMove.clear()
-
-            # Check diagonal down and right
-            myPieceFound = False
-            if X < self._boardSize - 2 and Y < self._boardSize - 2 and self._gameBoard[Y+1][X+1] == opp:
-                maxMve = min(self._boardSize-1-Y, self._boardSize-1-X)
-
-                for i in range(1, maxMve+1):
-                    if self._gameBoard[Y+i][X+i] == opp:
-                        tmpMove.append([Y+i,X+i])
-                    elif self._gameBoard[Y+i][X+i] == self._delimeter:
-                        break
-                    else:
-                        myPieceFound = True
-
-            if myPieceFound and tmpMove:
-                self.__tmpMove.extend(tmpMove)
-            tmpMove.clear()
-
-            # Check diagonal down and left
-            myPieceFound = False
-            if X > 1 and Y < self._boardSize - 2 and self._gameBoard[Y+1][X-1] == opp:
-                maxMve = min(self._boardSize-1-Y, X)
-
-                for i in range(1, maxMve+1):
-                    if self._gameBoard[Y+i][X-i] == opp:
-                        tmpMove.append([Y+i,X-i])
-                    elif self._gameBoard[Y+i][X-i] == self._delimeter:
-                        break
-                    else:
-                        myPieceFound = True
-
-            if myPieceFound and tmpMove:
-                self.__tmpMove.extend(tmpMove)
-            tmpMove.clear()
+            for dir in self.__dirList:
+                self.__isValidMoveDir(coord, dir, opp)
 
         return False if not self.__tmpMove else True
